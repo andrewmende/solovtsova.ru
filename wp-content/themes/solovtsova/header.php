81696d61d412af863414b565bee1806b3b9655d8
Gordey Kurakin <gordeykurakin@gmail.com>
2018-05-07 22:14:25 +0200

    Blackline on the top

--- wp-content/themes/solovtsova/header.php
+++ wp-content/themes/solovtsova/header.php
@@ -3,16 +3,17 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
-
-	<div id="sidebar" class="sidebar">
-		<header id="masthead" class="site-header" role="banner">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		</header><!-- .site-header -->
+	<header id="masthead" class="site-header" role="banner">
+		<div id="blackline" class="container-fluid">
+			yanasolovtsova@gmail.com
+		</div>
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		
+	</header><!-- .site-header -->
 
 	<div id="content" class="site-content">
