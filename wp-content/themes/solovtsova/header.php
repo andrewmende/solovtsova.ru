247d20dec8d924cc234ba758792527bf833a76da
amende <andrew.mende@booking.com>
2018-05-08 15:26:46 +0200

    Header and basis of main page

--- wp-content/themes/solovtsova/header.php
+++ wp-content/themes/solovtsova/header.php
@@ -3,16 +3,38 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 	<?php wp_head(); ?>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
 	<header id="masthead" class="site-header" role="banner">
-		<div id="blackline" class="container-fluid">
-			yanasolovtsova@gmail.com
+		<div id="blackline">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<a href="mailto:yanasolovtsova@gmail.com">yanasolovtsova@gmail.com</a>
+					</div>
+				</div>
+			</div>
 		</div>
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div id="site-branding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9 offset-md-3">
+						<div class="titles-group">
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<p class="site-subtitle"><?php bloginfo( 'description' ); ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="navigation">
+			Меню    |    Обо мне    |    Итд
+		</div>
+		
 		
 	</header><!-- .site-header -->
 
--- wp-content/themes/solovtsova/page.php
+++ wp-content/themes/solovtsova/page.php
@@ -13,26 +13,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area container">
+		<div id="main" class="site-main row" role="main">
+			<div class="col-md-3">
+				<h3>Процедуры</h3>
+			</div>
+			<div class="col-md-6">
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
 
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
+				// End the loop.
+				endwhile;
+				?>
+			</div>
+			<div class="col-md-3">
+				<h3>Проблемы</h3>
+			</div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		// End the loop.
-		endwhile;
-		?>
-
-		</main><!-- .site-main -->
+		</div><!-- .site-main -->
 	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
