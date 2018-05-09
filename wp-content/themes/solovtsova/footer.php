62ba15611ebe72abb4efc0eacc90907eae8a4fd0
Gordey Kurakin <gordeykurakin@gmail.com>
2018-05-09 18:05:01 +0200

    Footer on the bottom

--- wp-content/themes/solovtsova/footer.php
+++ wp-content/themes/solovtsova/footer.php
@@ -1,16 +1,30 @@
 	<hr />
-	<div id="footer" role="contentinfo">
+<div id="footer" role="contentinfo">
 	<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
-		<p>
-			<?php
-			printf(
-				/* translators: 1: blog name, 2: WordPress */
-				__( '%1$s is proudly powered by %2$s' ),
-				get_bloginfo( 'name' ),
-				'<a href="https://wordpress.org/">WordPress</a>'
-			);
-			?>
-		</p>
+	<div id=navigation>
+		<div class="container">
+			<div class="row">
+				<div id="navigation">
+					Меню    |    Обо мне    |    Итд
+				</div>
+			</div>
+		</div>	
+	</div>
+	<div class="container">
+		<div class="row">
+			<div id="footer_menu" class="col-md-3 offset-md-6">
+				<a href="mailto:yanasolovtsova@gmail.com">yanasolovtsova@gmail.com</a>
+				Черногория Будва, Бар
+			</div>
+		</div>
+	</div>
+	<div id="blackline">
+		<div class="container">
+			<div class="row">
+				<div id="blackline">
+				</div>
+			</div>
+		</div>
 	</div>
 </div> <!-- #content -->
 
