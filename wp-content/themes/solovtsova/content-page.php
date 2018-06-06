b40b710a86b43b3495e642a19fc1cad1046bf310
amende <andrew.mende@booking.com>
2018-06-06 21:53:15 +0200

    Removed page title.

--- wp-content/themes/solovtsova/content-page.php
+++ wp-content/themes/solovtsova/content-page.php
@@ -5,8 +5,5 @@
 ?>
 
 	<div class="entry-content">
-		<?php if (!is_front_page()) {
-			the_title($before = '<h1>', $after = '</h1>');	
-		} ?> 
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
