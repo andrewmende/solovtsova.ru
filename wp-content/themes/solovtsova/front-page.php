36356c4c2ba7cd2bce87679f31dde7da733a0408
amende <andrew.mende@booking.com>
2018-09-17 23:00:47 +0200

    Changes on main page to reflect new structure

--- wp-content/themes/solovtsova/front-page.php
+++ wp-content/themes/solovtsova/front-page.php
@@ -38,13 +38,15 @@ get_header(); ?>
 				// End the loop.
 				endwhile;
 				?>
+				<div class="action">
+    				<img src='/wp-content/themes/solovtsova/img/actions.png' class="img-fluid">
+				</div>
 				<div class="procedure-gallery">
-					<center><h1>Процедуры</h1></center>
 					<div>
 						<?php
 						// Filter through all pages and find Portfolio's children
 						$services_children = get_children(array(
-						   'post_parent' => 113, 
+						   'post_parent' => 1277, 
 						   'post_type'      => 'page',
 						   'numberposts' => 3,
 						   ));
@@ -84,17 +86,13 @@ get_header(); ?>
 	  <ol class="carousel-indicators">
 	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
 	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 	  </ol>
 	  <div class="carousel-inner">
 	    <div class="carousel-item active">
-	      <img class="d-block w-100" src="/wp-content/themes/solovtsova/img/slider-1.png" alt="First slide">
+	      <img class="d-block w-100" src="/wp-content/themes/solovtsova/img/slider-2.png" alt="First slide">
 	    </div>
 	    <div class="carousel-item">
-	      <img class="d-block w-100" src="/wp-content/themes/solovtsova/img/slider-2.png" alt="Second slide">
-	    </div>
-	    <div class="carousel-item">
-	      <img class="d-block w-100" src="/wp-content/themes/solovtsova/img/slider-3.png" alt="Third slide">
+	      <img class="d-block w-100" src="/wp-content/themes/solovtsova/img/slider-3.png" alt="Second slide">
 	    </div>
 	  </div>
 	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
